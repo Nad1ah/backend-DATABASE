d7628cde9a760ed4a5332be7e6c9408f8f0bd51f
GuilhermeMF <146479689+Nad1ah@users.noreply.github.com>
2024-02-09 21:57:11 +0000

    criacao de servidor e base de dados mongodb

---
+++ index.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require("mongodb");
+
+const dbUrl = process.env.DB_URL;
+
+const client = new MongoClient(dbUrl);
+
+async function main() {
+await client.connect();
+
+const db = client.db("EditDataBase");
+db.collection("tests").insertMany([{ name: "John" }, { name: "Alice" }]);
+console.log(db.collection("test").find().toArray()); 
+
+main()
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error(`Failed to connect to MongoDB: ${err}`)
+
+
+
